03891dd7372affcb3819ec8d0a59ca26b36166fa
Ted Callahan <CCallahanIV@gmail.com>
2016-12-06 13:29:31 -0800

    Wrote tests for next two fibonacci sequences, NOT base cases.  Beginning to test recursion.

--- src/test_series.py
+++ src/test_series.py
@@ -1,13 +1,15 @@
 """This is the test for the fibonacci and lucas number series in series.py."""
 import pytest
 
-# Expected answers for Fibonacci sequence
+# Expected answers for Fibonacci sequence.
 PARAMS_FIB = [
     (0, 0),
-    (1, 1)
+    (1, 1),
+    (2, 1),
+    (3, 2)
 ]
 
-# Expected answers for Lucas number sequence
+# Expected answers for Lucas number sequence.
 PARAMS_LUC = [
 
 ]
