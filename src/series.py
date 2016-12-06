4389108eb05fd7942f5dfa882961e7c330064dee
Ted Callahan <CCallahanIV@gmail.com>
2016-12-06 13:46:32 -0800

    Completed lucas function function has same recursive pattern as fibonacci but different starting terms.

--- src/series.py
+++ src/series.py
@@ -1,11 +1,21 @@
 """This code houses the Fibonacci and Lucas functions."""
 
+
 def fibonacci(n):
+    """Return the nth term of the fibonacci sequence."""
     if n == 0:
         return 0
     if n == 1:
         return 1
     else:
-        return fibonacci(n-1) + fibonacci(n-2)
+        return fibonacci(n - 1) + fibonacci(n - 2)
 
-#def lucas(n):
+
+def lucas(n):
+    """Return the nth term of the Lucas sequence."""
+    if n == 0:
+        return 2
+    if n == 1:
+        return 1
+    else:
+        return lucas(n - 2) + lucas(n - 1)
