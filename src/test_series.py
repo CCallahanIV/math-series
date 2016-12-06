20e7a9d951f8184eaed6eaddcf03bdd2b83e5da8
Ted Callahan <CCallahanIV@gmail.com>
2016-12-06 13:14:29 -0800

    Test format written, first test prepared.

---
+++ src/test_series.py
@@ -0,0 +1,19 @@
+"""This is the test for the fibonacci and lucas number series in series.py."""
+import pytest
+
+# Expected answers for Fibonacci sequence
+PARAMS_FIB = [
+    (0, 0),
+    (1, 1)
+]
+
+# Expected answers for Lucas number sequence
+PARAMS_LUC = [
+
+]
+
+@pytest.mark.parametrize("n, result", PARAMS_FIB)
+def test_fibonacci(n, result):
+    """Test the fibonacci function."""
+    from series import fibonacci
+    assert fibonacci(n) == result
